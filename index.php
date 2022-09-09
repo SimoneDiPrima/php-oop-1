96064dda898b65f614f8781e4a47f96b9c2a0a84
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-09 17:16:03 +0200

    definizione costruttore e stampa a schermo

--- index.php
+++ index.php
@@ -7,15 +7,158 @@
 <?php 
 class Movies
 {
-    public $name;
+    public $id;
+    public $title;
     public $originalname;
     public $genre;
     public $year;
-    public $language;
 
+    public function __construct($_id,$_title,$_originalname,$_genre,$_year)
+    {
+        $this->id=$_id;
+        $this->title=$_title;
+        $this->originalname=$_originalname;
+        $this->genre=$_genre;
+        $this->year=$_year;
+        
+    }
 }
 
-$moviesDrama = new Movies()
-$moviesComedy = new Movies()
-$moviesScifi = new Movies()
-$moviesDocumentary = new Movies()
+$movie_comedy = new Movies(1, "UNA NOTTE DA LEONI","one night from lions","Comedy",2012);
+$movie_drama = new Movies(2, "IL MIGLIO VERDE","the green mile","drama",1992);
+$movie_documentary = new Movies(3, "THE SOCIAL DILEMMA","the social dilemma","documentary",2020);
+$movie_thriller = new Movies(4, "IL SILENZIO DEGLI INNOCENTI","THE BUTTERFLY EFFECT","THRILLER",1988);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <ul>
+            <li>
+                <h5>
+                <?php echo $movie_comedy->id ?> 
+                </h5>
+            </li>
+            <li>
+                <h2>
+                <?php echo $movie_comedy->title ?>
+                </h2>
+            </li>
+            <li>
+                <h3>
+                <?php echo $movie_comedy->originalname ?>
+                </h3>
+            </li>
+            <li>
+                <h4>
+                <?php echo $movie_comedy->genre ?>
+                </h4>
+            </li>
+            <li>
+                <h6>
+                <?php echo $movie_comedy->year ?>
+                </h6>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <h5>
+                <?php echo $movie_drama->id ?> 
+                </h5>
+            </li>
+            <li>
+                <h2>
+                <?php echo $movie_drama->title ?>
+                </h2>
+            </li>
+            <li>
+                <h3>
+                <?php echo $movie_drama->originalname ?>
+                </h3>
+            </li>
+            <li>
+                <h4>
+                <?php echo $movie_drama->genre ?>
+                </h4>
+            </li>
+            <li>
+                <h6>
+                <?php echo $movie_drama->year ?>
+                </h6>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <h5>
+                <?php echo $movie_documentary->id ?> 
+                </h5>
+            </li>
+            <li>
+                <h2>
+                <?php echo $movie_documentary->title ?>
+                </h2>
+            </li>
+            <li>
+                <h3>
+                <?php echo $movie_documentary->originalname ?>
+                </h3>
+            </li>
+            <li>
+                <h4>
+                <?php echo $movie_documentary->genre ?>
+                </h4>
+            </li>
+            <li>
+                <h6>
+                <?php echo $movie_documentary->year ?>
+                </h6>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <h5>
+                <?php echo $movie_thriller->id ?> 
+                </h5>
+            </li>
+            <li>
+                <h2>
+                <?php echo $movie_thriller->title ?>
+                </h2>
+            </li>
+            <li>
+                <h3>
+                <?php echo $movie_thriller->originalname ?>
+                </h3>
+            </li>
+            <li>
+                <h4>
+                <?php echo $movie_thriller->genre ?>
+                </h4>
+            </li>
+            <li>
+                <h6>
+                <?php echo $movie_thriller->year ?>
+                </h6>
+            </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
+
+  
